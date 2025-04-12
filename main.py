0ebce2514efae6f29479be9fb233f44e30027170
myonekawa1090 <bewith.1090@gmail.com>
2025-04-11 22:04:55 -0700

    add player.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 # throughout this file
 import pygame   
 from constants import *
-
+from player import Player
 
 def main():
     print("Starting Asteroids!")
@@ -13,13 +13,16 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))  # create a window
     clock = pygame.time.Clock()  # create a clock object to control the frame rate
     dt = 0  # delta time, the time since the last frame
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)  # create a player object
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         pygame.Surface.fill(screen, (0, 0, 0))
+        player.draw(screen)  # draw the player
+        player.update(dt)  # update the player
         pygame.display.flip()  # update the display
-        clock.tick(60)
+        dt = clock.tick(60) / 1000  # limit the frame rate to 60 FPS
 
 
 if __name__ == "__main__":
