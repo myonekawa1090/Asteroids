23de381b3d8c2bbc51ab3c876994320d1fef8f5a
myonekawa1090 <bewith.1090@gmail.com>
2025-04-11 21:28:01 -0700

    add display function

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
 # this allows us to use code from
 # the open-source pygame library
 # throughout this file
-import pygame
+import pygame   
+from constants import *
+
 
 def main():
     print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    pygame.init()  # initialize the pygame library
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))  # create a window
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        pygame.Surface.fill(screen, (0, 0, 0))
+        pygame.display.flip()  # update the display
+        
+
 
 if __name__ == "__main__":
     main()
